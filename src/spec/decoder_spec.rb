91c370755327a06f44026e6911c3ba92a91e9fac
rebuilt <memoryman51@hotmail.com>
2025-06-13 13:03:16 -0700

    add rspec and tests

--- src/spec/decoder_spec.rb
+++ src/spec/decoder_spec.rb
@@ -1,17 +1,22 @@
-require 'rspec/autorun'
+require_relative '../decoder'
 
-RSpec.describe 'Decoder output' do
+RSpec.describe '#decode', :model do
   it 'matches the output of ndisasm for many register movs' do
-    ndisasm_output = `ndisasm -b 16 many_register_movs`
-    ruby_output = `ruby decoder.rb many_register_movs`
+    filename = 'many_register_movs'
+    ndisasm_output = `ndisasm -b 16 #{filename}`
+    instructions = ndisasm_output.split("\n").map { |line| line.split(' ')[2..].join(' ') + "\n" }.join
+    ruby_output = `ruby decoder.rb #{filename}`
 
-    expect(ruby_output).to eq(ndisasm_output)
+    expect(ruby_output).to eq(instructions)
   end
 
   it 'matches the ouput for immediate to register movs' do
-    ndisasm_output = `ndisasm -b 16 immediate_to_register_movs`
-    ruby_output = `ruby decoder.rb immediate_to_register_movs`
+    filename = 'immediate_to_register'
+    ndisasm_output = `ndisasm -b 16 #{filename}`
+    instructions = ndisasm_output.split("\n").map { |line| line.split(' ')[2..].join(' ') + "\n" }.join
 
-    expect(ruby_output).to eq(ndisasm_output)
+    ruby_output = `ruby decoder.rb #{filename}`
+
+    expect(ruby_output).to eq(instructions)
   end
 end
