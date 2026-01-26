32c1133a2ea95cd3348fa8294e065a2d25abee4a
rebuilt <memoryman51@hotmail.com>
2026-01-26 14:54:27 -0800

    get negative numbers working for 8 and 16 bit immediate to register movs

--- decoder2.rb
+++ decoder2.rb
@@ -1,59 +1,59 @@
-require "byebug"
+require 'byebug'
 
 EIGHT_BIT_REGISTERS = {
-  0b000 => "al",
-  0b010 => "dl",
-  0b001 => "cl",
-  0b011 => "bl",
-  0b100 => "ah",
-  0b111 => "bh",
-  0b101 => "ch",
-  0b110 => "dh"
+  0b000 => 'al',
+  0b010 => 'dl',
+  0b001 => 'cl',
+  0b011 => 'bl',
+  0b100 => 'ah',
+  0b111 => 'bh',
+  0b101 => 'ch',
+  0b110 => 'dh'
 
 }
 
 SIXTEEN_BIT_REGISTERS = {
-  0b000 => "ax",
-  0b011 => "bx",
-  0b001 => "cx",
-  0b010 => "dx",
-  0b100 => "sp",
-  0b101 => "bp",
-  0b110 => "si",
-  0b111 => "di"
+  0b000 => 'ax',
+  0b011 => 'bx',
+  0b001 => 'cx',
+  0b010 => 'dx',
+  0b100 => 'sp',
+  0b101 => 'bp',
+  0b110 => 'si',
+  0b111 => 'di'
 }
 
 EFFECTIVE_ADDRESS_MODE_00 = {
-  0b000 => "bx+si",
-  0b001 => "bx+di",
-  0b010 => "bp+si",
-  0b011 => "bp+di",
-  0b100 => "si",
-  0b101 => "di",
-  0b110 => "Direct address",
-  0b111 => "bx"
+  0b000 => 'bx+si',
+  0b001 => 'bx+di',
+  0b010 => 'bp+si',
+  0b011 => 'bp+di',
+  0b100 => 'si',
+  0b101 => 'di',
+  0b110 => 'Direct address',
+  0b111 => 'bx'
 }
 
 EFFECTIVE_ADDRESS_MODE_01 = {
-  0b000 => "bx+si",
-  0b001 => "bx+di",
-  0b010 => "bp+si",
-  0b011 => "bp+di",
-  0b100 => "si",
-  0b101 => "di",
-  0b110 => "bp",
-  0b111 => "bx"
+  0b000 => 'bx+si',
+  0b001 => 'bx+di',
+  0b010 => 'bp+si',
+  0b011 => 'bp+di',
+  0b100 => 'si',
+  0b101 => 'di',
+  0b110 => 'bp',
+  0b111 => 'bx'
 }
 
 EFFECTIVE_ADDRESS_MODE_10 = {
-  0b000 => "bx+si",
-  0b001 => "bx+di",
-  0b010 => "bp+si",
-  0b011 => "bp+di",
-  0b100 => "si",
-  0b101 => "di",
-  0b110 => "bp",
-  0b111 => "bx"
+  0b000 => 'bx+si',
+  0b001 => 'bx+di',
+  0b010 => 'bp+si',
+  0b011 => 'bp+di',
+  0b100 => 'si',
+  0b101 => 'di',
+  0b110 => 'bp',
+  0b111 => 'bx'
 }
 
 # first byte    | second byte | third byte
@@ -61,11 +61,11 @@ EFFECTIVE_ADDRESS_MODE_10 = {
 
 module ImmediateToRegister
   def self.get_opcode(bytes, index)
-    opcode = ""
+    opcode = ''
     case bytes[index]
     # 1 0 1 1 w reg
     when 0b10110000..0b10111111
-      opcode = "mov"
+      opcode = 'mov'
     end
     opcode
   end
@@ -79,16 +79,25 @@ module ImmediateToRegister
   def self.get_reg(bytes, index, w)
     # 1 0 1 1 w reg
     register_code = bytes[index] & 0b111
-    register_type = (w == 0) ? EIGHT_BIT_REGISTERS : SIXTEEN_BIT_REGISTERS
+    register_type = w == 0 ? EIGHT_BIT_REGISTERS : SIXTEEN_BIT_REGISTERS
     register_type[register_code]
   end
 
   def self.get_data(bytes, index, w)
+    byebug
     # data  | data if w == 1
     if w == 0
-      bytes[index]
-    else
-      ((bytes[index] << 8) | bytes[index])
+      if bytes[index] >> 7 & 1 == 1
+        bytes[index] - 256
+      else
+        bytes[index]
+      end
+    elsif w == 1
+      if bytes[index + 1] >> 7 & 1 == 1
+        ((bytes[index + 1] << 8) | bytes[index]) - 65_536
+      else
+        (bytes[index + 1] << 8) | bytes[index]
+      end
     end
   end
 
@@ -98,8 +107,8 @@ module ImmediateToRegister
     reg = get_reg(bytes, index, w)
     data = get_data(bytes, index + 1, w)
     output = "#{opcode} #{reg}, #{data}"
-    index = (w == 0) ? index + 2 : index + 3
-    [output, index]
+    increment = w == 0 ? 2 : 3
+    [output, increment]
   end
 end
 
@@ -110,7 +119,7 @@ module RegisterOrMemoryToOrFromRegister
   def self.opcode(bytes, index)
     case bytes[index]
     when 0b10001000..0b10001011 # Register/memory to/from register
-      "mov"
+      'mov'
     end
   end
 
@@ -166,7 +175,7 @@ module RegisterOrMemoryToOrFromRegister
     when 0b10
       EFFECTIVE_ADDRESS_MODE_10
     when 0b11
-      (w == 0) ? EIGHT_BIT_REGISTERS : SIXTEEN_BIT_REGISTERS
+      w == 0 ? EIGHT_BIT_REGISTERS : SIXTEEN_BIT_REGISTERS
     end
   end
 
@@ -174,7 +183,7 @@ module RegisterOrMemoryToOrFromRegister
     "#{opcode} #{destination},#{source} #{displacement}"
   end
 
-  def self.increment(mod_code, rm_code)
+  def self.increment(mod_code, rm_code, w)
     case [mod_code, rm_code] # Direct address, 16 bit displacement
     in [0b0, 0b110]
       2
@@ -184,6 +193,8 @@ module RegisterOrMemoryToOrFromRegister
       4
     in [0b11, _]
       2
+    in [0b0, _]
+      w == 0 ? 2 : 3
     end
   end
 
@@ -207,52 +218,61 @@ module RegisterOrMemoryToOrFromRegister
     rtype = register_type(w)
     eaddress = effective_address(mcode, w)
 
+    # source = d.zero? ? rtype[rcode] : eaddress[rmcode]
+    # destination = d.zero? ? eaddress[rmcode] : rtype[rcode]
+    #
+    # source = "+0x#{bytes[index + 2].to_s(16)}" if mcode == 0b00 && rmcode == 0b110 # Direct address, 16 bit displacement
+    #
+    # if mcode == 0b01 # Direct address, 8 bit displacement
+    #   displacement = "+0x#{bytes[index + 2].to_s(16)}"
+    #   d.zero? ? destination += displacement : source += displacement
+    # end
+    #
+    # if mcode == 0b10 # Direct address, 8 bit displacement
+    #   displacement = "+0x#{bytes[index + 2..index + 3].pack('C*').unpack1('v').to_s(16)}"
+    #
+    #   d.zero? ? destination += displacement : source += displacement
+    # end
+    #
+    # if mcode != 0b11
+    #   d.zero? ? destination = "[#{destination}]" : source = "[#{source}]"
+    # end
+    # instruct = "#{o} #{destination}, #{source}"
+
     source = d.zero? ? rtype[rcode] : eaddress[rmcode]
     destination = d.zero? ? eaddress[rmcode] : rtype[rcode]
-
-    source = "+0x#{bytes[index + 2].to_s(16)}" if mcode == 0b00 && rmcode == 0b110 # Direct address, 16 bit displacement
-
-    if mcode == 0b01 # Direct address, 8 bit displacement
-      displacement = "+0x#{bytes[index + 2].to_s(16)}"
-      d.zero? ? destination += displacement : source += displacement
+    case mcode
+    when 0b00
+      source = "+0x#{bytes[index + 2].to_s(16)}" if rmcode == 0b110 # Direct address, 16 bit displacement
+    when 0b11
+      # no additional processing needed
+    else
+      instruct = 'not implemented yet'
     end
-
-    if mcode == 0b10 # Direct address, 8 bit displacement
-      displacement = "+0x#{bytes[index + 2..index + 3].pack("C*").unpack1("v").to_s(16)}"
-
-      d.zero? ? destination += displacement : source += displacement
-    end
-
-    if mcode != 0b11
-      d.zero? ? destination = "[#{destination}]" : source = "[#{source}]"
-    end
-    instruct = "#{o} #{destination},#{source}"
-    byebug
-    [instruct, increment(mcode, rmcode)]
+    instruct ||= "#{o} #{destination},#{source}"
+    [instruct, increment(mcode, rmcode, w)]
   end
 end
 
 binary_file = ARGV[0]
-binary_file ||= "./asm/register_movs"
+binary_file ||= './asm/register_movs'
 
 bytes = File.binread(binary_file).bytes
 puts bytes.length
 index = 0
-output = ""
+output = ''
 while index < bytes.length
   case bytes[index]
   when 0b10110000..0b10111111
     instruction, idx = ImmediateToRegister.decode(bytes, index)
     output += instruction + "\n"
     index += idx
-    byebug
   when 0b100010..0b10001011
     instruction, idx = RegisterOrMemoryToOrFromRegister.decode(bytes, index)
     output += instruction + "\n"
     index += idx
-    byebug
   when 0b0..0o11111111
-    puts "#{bytes[index].to_s(2).rjust(8, "0")} not implemented"
+    puts "#{bytes[index].to_s(2).rjust(8, '0')} not implemented"
     break
   end
 end
