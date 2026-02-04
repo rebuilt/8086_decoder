617a6c3612fa0259acd3306240517e9cdbff63da
rebuilt <memoryman51@hotmail.com>
2026-02-04 10:24:55 -0800

    Decode to/from segment registers

--- decoder2.rb
+++ decoder2.rb
@@ -56,21 +56,56 @@ EFFECTIVE_ADDRESS_MODE_10 = {
   0b111 => 'bx'
 }
 
+SR = {
+  0b00 => 'es',
+  0b01 => 'cs',
+  0b10 => 'ss',
+  0b11 => 'ds'
+}
+
+def direction(bytes, index)
+  # Extract destination from bytes at given index
+  # 100010 d w
+  (bytes[index] >> 1) & 0b1
+end
+
+def width(bytes, index)
+  # 100010 d w
+  bytes[index] & 0b1
+end
+
+def get_data(bytes, index, w)
+  # data  | data if w == 1
+  if w == 0
+    if bytes[index] >> 7 & 1 == 1
+      bytes[index] - 256
+    else
+      bytes[index]
+    end
+  elsif w == 1
+    if bytes[index + 1] >> 7 & 1 == 1
+      ((bytes[index + 1] << 8) | bytes[index]) - 65_536
+    else
+      (bytes[index + 1] << 8) | bytes[index]
+    end
+  end
+end
+
+def rm_code(bytes, index)
+  # mod reg r/m
+  bytes[index] & 0b111
+end
+
+def mod_code(bytes, index)
+  # mod reg r/m
+  bytes[index] >> 6
+end
+
 # first byte    | second byte | third byte
 # 1 0 1 1 w reg | data        | data if w == 1
 
 module ImmediateToRegister
   class << self
-    def get_opcode(bytes, index)
-      opcode = ''
-      case bytes[index]
-      # 1 0 1 1 w reg
-      when 0b10110000..0b10111111
-        opcode = 'mov'
-      end
-      opcode
-    end
-
     def get_width(bytes, index)
       # 1 0 1 1 w reg
       # (bytes[index] >> 3)  & 0b1
@@ -84,25 +119,7 @@ module ImmediateToRegister
       register_type[register_code]
     end
 
-    def get_data(bytes, index, w)
-      # data  | data if w == 1
-      if w == 0
-        if bytes[index] >> 7 & 1 == 1
-          bytes[index] - 256
-        else
-          bytes[index]
-        end
-      elsif w == 1
-        if bytes[index + 1] >> 7 & 1 == 1
-          ((bytes[index + 1] << 8) | bytes[index]) - 65_536
-        else
-          (bytes[index + 1] << 8) | bytes[index]
-        end
-      end
-    end
-
-    def decode(bytes, index)
-      opcode = get_opcode(bytes, index)
+    def decode(opcode, bytes, index)
       w = get_width(bytes, index)
       reg = get_reg(bytes, index, w)
       data = get_data(bytes, index + 1, w)
@@ -118,39 +135,9 @@ end
 # 100010 d w | mod reg r/m | (DISP-LO)  | (DISP-HI)
 module RegisterOrMemoryToOrFromRegister
   class << self
-    def opcode(bytes, index)
-      case bytes[index]
-      when 0b10001000..0b10001011
-        'mov'
-      when 0b000000..0b00000011
-        'add'
-      end
-    end
-
-    def direction(bytes, index)
-      # Extract destination from bytes at given index
-      # 100010 d w
-      (bytes[index] >> 1) & 0b1
-    end
-
-    def width(bytes, index)
-      # 100010 d w
-      bytes[index] & 0b1
-    end
-
-    def mod_code(bytes, index)
-      # mod reg r/m
-      bytes[index] >> 6
-    end
-
     def reg_code(bytes, index)
       # mod reg r/m
       bytes[index] >> 3 & 0b111
-    end
-
-    def rm_code(bytes, index)
-      # mod reg r/m
-      bytes[index] & 0b111
     end
 
     def register_type(w)
@@ -203,8 +190,7 @@ module RegisterOrMemoryToOrFromRegister
       end
     end
 
-    def decode(bytes, index)
-      o = opcode(bytes, index)
+    def decode(opcode, bytes, index)
       d = direction(bytes, index)
       w = width(bytes, index)
 
@@ -227,9 +213,108 @@ module RegisterOrMemoryToOrFromRegister
       when 0b11
         # no additional processing needed
       end
-      instruct = "#{o} #{destination},#{source}"
+      instruct = "#{opcode} #{destination},#{source}"
       inc = increment(mcode, rmcode, w)
       # byebug
+      [instruct, inc]
+    end
+  end
+end
+
+module MemoryToOrFromAccumulator
+  class << self
+    def increment(width)
+      width == 0 ? 2 : 3
+    end
+
+    def decode(opcode, bytes, index)
+      # to be implemented
+      d = direction(bytes, index)
+      w = width(bytes, index)
+      data = get_data(bytes, index + 1, w)
+
+      destination = d.zero? ? 'ax' : "[#{data}]"
+      source = d.zero? ? "[#{data}]" : 'ax'
+      instruct = "#{opcode} #{destination},#{source}"
+      inc = increment(w)
+      # byebug
+      [instruct, inc]
+    end
+  end
+end
+
+module RegisterOrMemoryToOrFromSegmentRegister
+  class << self
+    def get_sr(bytes, index)
+      # mod 0 sr r/m
+      bytes[index] >> 3 & 0b11
+    end
+
+    def effective_address(mod_code)
+      case mod_code
+      when 0b00
+        EFFECTIVE_ADDRESS_MODE_00
+      when 0b01
+        EFFECTIVE_ADDRESS_MODE_01
+      when 0b10
+        EFFECTIVE_ADDRESS_MODE_10
+      when 0b11
+        SIXTEEN_BIT_REGISTERS
+      end
+    end
+
+    def displacement(bytes, index, mod_code)
+      case mod_code
+      when 0b01
+        bytes[index]
+      when 0b10
+        bytes[index + 1] << 8 | bytes[index]
+      else
+        0
+      end
+    end
+
+    def increment(mod_code)
+      case mod_code
+      in 0b00
+        2
+      in 0b01
+        3
+      in 0b10
+        4
+      in 0b11
+        2
+      end
+    end
+
+    def decode(opcode, bytes, index)
+      # to be implemented
+      d = direction(bytes, index)
+
+      srcode = get_sr(bytes, index + 1)
+      rmcode = rm_code(bytes, index + 1)
+      mcode = mod_code(bytes, index + 1)
+      eaddress = effective_address(mcode)
+
+      destination = d.zero? ? eaddress[rmcode] : SR[srcode]
+      source = d.zero? ? SR[srcode] : eaddress[rmcode]
+
+      case mcode
+      in 0b00
+        d.zero? ? destination = "[#{destination}]" : source = "[#{source}]"
+      in 0b01..0b10
+        disp = displacement(bytes, index + 2, mcode)
+        if d.zero?
+          destination = "[#{destination} + #{disp}]"
+        else
+          source = "[#{source} + #{disp}]"
+        end
+      in 0b11
+        # no additional processing needed
+      end
+
+      instruct = "#{opcode} #{destination},#{source}"
+      inc = increment(mcode)
       [instruct, inc]
     end
   end
@@ -242,24 +327,26 @@ bytes = File.binread(binary_file).bytes
 puts bytes.length
 index = 0
 output = ''
+
 while index < bytes.length
   case bytes[index]
   when 0b10110000..0b10111111
-    instruction, idx = ImmediateToRegister.decode(bytes, index)
-    output += instruction + "\n"
-    index += idx
+    instruction, idx = ImmediateToRegister.decode('mov', bytes, index)
   when 0b100010..0b10001011
-    instruction, idx = RegisterOrMemoryToOrFromRegister.decode(bytes, index)
-    output += instruction + "\n"
-    index += idx
+    instruction, idx = RegisterOrMemoryToOrFromRegister.decode('mov', bytes, index)
+  when 0b10100000..0b10100011
+    instruction, idx = MemoryToOrFromAccumulator.decode('mov', bytes, index)
+  when 0b10001110, 0b10001100
+    instruction, idx = RegisterOrMemoryToOrFromSegmentRegister.decode('mov', bytes, index)
   when 0b000000..0b00000011
-    instruction, idx = RegisterOrMemoryToOrFromRegister.decode(bytes, index)
-    output += instruction + "\n"
-    index += idx
+    instruction, idx = RegisterOrMemoryToOrFromRegister.decode('add', bytes, index)
   when 0b0..0o11111111
     puts "#{bytes[index].to_s(2).rjust(8, '0')} not implemented"
     break
   end
+
+  output += instruction + "\n"
+  index += idx
 end
 
 puts output
